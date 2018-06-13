0eda91db906faa6b5f0cfce9fb3a4a4c256a08b7
Manish <memanishindian@gmail.com>
2018-06-13 21:59:43 +0530

    added the services to heroesComponent which will show the dtails

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import {Hero} from '../hero';
-import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +9,19 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
- 
-  heroes = HEROES;
- 
   selectedHero: Hero;
+  heroes:Hero[];
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
  
  
-  constructor() { }
+  constructor(private heroService: HeroService) { 
+   
+  }
  
   ngOnInit() {
+    this.getHeroes();
   }
  
   onSelect(hero: Hero): void {
